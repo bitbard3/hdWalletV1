887577e4b7431434d665247169185c7a7fc9323c
bitbard3 <ansharora3839@gmail.com>
2024-10-17 14:59:23 +0530

    Add blockchain state and account state

--- src/features/authSlicer.ts
+++ src/features/authSlicer.ts
@@ -1,11 +1,16 @@
+import { BlockchainConfig } from "@/constants/blockchainConfig";
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 export interface AuthState {
   currentStep: number;
+  blockchain: keyof BlockchainConfig | null;
+  account:number;
 }
 
 const initialState: AuthState = {
   currentStep: 1,
+  blockchain: null,
+  account:0
 };
 
 export const authSlice = createSlice({
@@ -15,8 +20,17 @@ export const authSlice = createSlice({
     updateStep: (state, action: PayloadAction<number>) => {
       state.currentStep = action.payload;
     },
+    updateBlockchain: (
+      state,
+      action: PayloadAction<keyof BlockchainConfig>
+    ) => {
+      state.blockchain = action.payload;
+    },
+    updateAccount: (state, action: PayloadAction<number>) => {
+      state.account = action.payload;
+    },
   },
 });
 
-export const { updateStep } = authSlice.actions;
+export const { updateStep,updateBlockchain,updateAccount } = authSlice.actions;
 export default authSlice.reducer;
