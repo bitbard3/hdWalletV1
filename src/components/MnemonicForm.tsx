d38f4eeb4e6c2295915581545008cfaca5110106
bitbard3 <ansharora3839@gmail.com>
2024-10-20 20:44:19 +0530

    Add Mnemonic form

---
+++ src/components/MnemonicForm.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { generateMnemonic } from "bip39";
+import { Buffer } from "buffer";
+import PrimaryButton from "./PrimaryButton";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/app/store";
+import {
+  updateStep,
+  updateMnenomic,
+  addAccount,
+  updateAccount,
+} from "@/features/authSlicer";
+import { generateWallet } from "@/lib/utlils/generateWallet";
+window.Buffer = Buffer;
+
+export default function MnemonicForm() {
+  const dispatch = useDispatch();
+  const currentStep = useSelector((state: RootState) => state.auth.currentStep);
+  const blockchain = useSelector((state: RootState) => state.auth.blockchain);
+  const account = useSelector((state: RootState) => state.auth.account);
+  const mnemonicFromState = useSelector(
+    (state: RootState) => state.auth.mnemonic
+  );
+  const [mnemonicPhrase, setMnemonicPhrase] = useState<string>("");
+
+  useEffect(() => {
+    if (!mnemonicFromState) {
+      const newMnemonic = generateMnemonic();
+      setMnemonicPhrase(newMnemonic);
+      dispatch(updateMnenomic(newMnemonic));
+    } else {
+      setMnemonicPhrase(mnemonicFromState);
+    }
+  }, [mnemonicFromState, dispatch]);
+
+  const mnemonicArr = mnemonicPhrase.split(" ");
+
+  const onNextHandler = async () => {
+    dispatch(updateStep(currentStep + 1));
+    if (account === 0) {
+      const keys = await generateWallet(mnemonicPhrase, blockchain);
+      dispatch(addAccount(keys));
+      dispatch(updateAccount(account + 1));
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <p className="text-light font-medium text-3xl">Secret Recovery Phrase</p>
+      <p className="mt-4 text-xl text-yellow-500">
+        Save these words to recover your wallet
+      </p>
+      <div className="w-full grid grid-cols-3 gap-3 mt-7">
+        {mnemonicArr.map((word, index) => (
+          <div
+            key={index}
+            className="px-1 py-2 bg-gray-100 rounded shadow text-center"
+          >
+            {index + 1}. {word}
+          </div>
+        ))}
+      </div>
+      <div className="mt-10 pb-4 w-full">
+        <PrimaryButton text="Generate Wallet" onClick={onNextHandler} />
+      </div>
+    </div>
+  );
+}
