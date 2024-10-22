c3cfa6201728259c819e5d2657df32f42962d1ba
bitbard3 <ansharora3839@gmail.com>
2024-10-22 14:32:53 +0530

    Create copy to clipboard helper funciton

--- src/lib/utlils/utils.ts
+++ src/lib/utlils/utils.ts
@@ -8,3 +8,16 @@ export function cn(...inputs: ClassValue[]) {
 export const capitaliseString = (word: string): string => {
   return word.charAt(0).toUpperCase() + word.slice(1);
 };
+
+export const copyToClipboard =async (text:string):Promise<void> =>{
+ if(!navigator.clipboard){
+  throw new Error("Clipbboard API not available")
+ }
+ try{
+  await navigator.clipboard.writeText(text)
+ }
+ catch(err){
+  console.error("Failed to copy text: ",err)
+  throw new Error("Something went wrong")
+ }
+}
