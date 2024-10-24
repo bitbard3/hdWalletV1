35cefcd1b269ee8cc78aa538323b697c658ffa88
bitbard3 <ansharora3839@gmail.com>
2024-10-24 21:02:44 +0530

    Fix: Disallow users to jump from steps without generating wallet in stepform

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,13 +1,15 @@
 import { RootState } from "@/app/store";
 import { updateStep } from "@/features/authSlicer";
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProgressSlide from "./ProgressSlide";
 import { STEPS } from "@/constants/constants";
 
 export default function ProgressBar() {
   const currentStep = useSelector((state: RootState) => state.auth.currentStep);
+  const keys = useSelector((state: RootState) => state.auth.keys);
   const dispatch = useDispatch();
+  const [stepsDisabled, setStepsDisabled] = useState<boolean>(true);
 
   const handleProgressSlideClick = (e: SyntheticEvent) => {
     e.preventDefault();
@@ -16,12 +18,19 @@ export default function ProgressBar() {
     dispatch(updateStep(clickedStep));
   };
 
+  useEffect(() => {
+    if (keys.length > 0) {
+      setStepsDisabled(false);
+    }
+  }, [keys.length]);
+
   return (
     <div className="pt-10 flex justify-between w-full">
       {Array.from({ length: STEPS }, (_, index) => {
         const stepNumber = index + 1;
         return (
           <ProgressSlide
+            disabled={stepsDisabled}
             isFilled={stepNumber <= currentStep}
             handleClick={handleProgressSlideClick}
             progress={stepNumber}
--- src/components/ProgressSlide.tsx
+++ src/components/ProgressSlide.tsx
@@ -3,15 +3,18 @@ type ProgressSlideProps = {
   handleClick: (event: SyntheticEvent) => void;
   isFilled: boolean;
   progress: number;
+  disabled:boolean
 };
 
 export default function ProgressSlide({
   handleClick,
   isFilled,
   progress,
+  disabled
 }: ProgressSlideProps) {
   return (
     <button
+    disabled={disabled}
       data-progress={`${progress}`}
       onClick={handleClick}
       className={`h-1 w-[30%] rounded-full ${
